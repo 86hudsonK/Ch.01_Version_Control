08d1eab136db78462364df1912a8ef6498ac3409
86hudsonK <kerchalhud@urbandaleschools.com>
2024-08-29 13:31:54 -0500

    Update 1.0_Jedi_Training.py

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -26,28 +26,31 @@ The last line keeps the window open until you click to close.
 Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=turtle
 '''
 import turtle
-yoda=turtle.Turtle()
-screen=turtle.Screen() # makes a screen object
-screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
+t=turtle.Turtle()
+s=turtle.Screen()
+s.bgcolor('black')
+t.pencolor('lavender')
+t.color('lavender')
+a=0
+b=0
+t.speed(0)
+t.penup()
+t.goto(-10,180)
+t.pendown()
+while True:
+  t.forward(a)
+  t.right(b)
+  a+=3
+  b+=1
+  if b ==210:
+    break
+  t.hideturtle()
+t.penup()
+t.goto(-195,180)
+t.write('Hudson Kerchal')
+turtle.done()
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+
+
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
